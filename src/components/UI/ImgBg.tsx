e45d7c4b7e68cb3daa46f9363a568e26e5810349
larioscow <christiandlf15@gmail.com>
2025-09-09 18:40:12 -0600

    asdfasdfa

--- src/components/UI/ImgBg.tsx
+++ src/components/UI/ImgBg.tsx
@@ -1,7 +1,7 @@
 export const ImgBg = () => {
   return (
     <div className="flex justify-center items-center h-[calc(100dvh-72px)] absolute">
-      <div className="rounded-4xl grid grid-cols-1 place-items-center absolute top-0 z-[-10] w-[calc(100vw-20px)] sm:w-[calc(100vw-40px)] h-[calc(100vh-100px)] overflow-clip animate-in">
+      <div className="rounded-4xl grid grid-cols-1 place-items-center absolute top-0 z-[-10] w-[calc(100vw-20px)] sm:w-[calc(100vw-40px)] h-[calc(100dvh-100px)] overflow-clip animate-in">
         <div className="bg-blue-600 rounded-full size-[100vw] scale-[125%] md:scale-[100%] -translate-x-[50%] -translate-y-[100%] md:-translate-y-[50%] absolute blur"></div>
         <div className="bg-amber-600 rounded-full size-[100vw] scale-[110%] md:scale-[100%]  translate-x-7/12 translate-y-5/12 md:translate-y-6/12 absolute blur">
           <div className="bg-black rounded-full size-[100vw] scale-[60%]"></div>
